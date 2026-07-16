bcc247f30b60a956e9239930c7b2cdeaafc0893a
Ali Ansari <badnan541@gmail.com>
2026-07-17 00:01:43 +0100

    Removed test code from start_game() and pasted it into criteria.txt. I will develop a game loop inside start_game() next, for stage 1 and beyond.

--- main.py
+++ main.py
@@ -23,26 +23,7 @@ def create_player():
 
 def start_game():
 
-    #create entities
-    player = ch.Player(create_player())
-    chest = obj.Chest()
-    door = obj.Door()
-    star = obj.Star()
-
-    #test the door when its locked
-    door.interact(player) #should not allow as player doesn't have a key
-
-    #open the chest
-    chest.interact(player) #player finds key
-
-    #try opening chest again
-    chest.interact(player) #no key anymore, its empty
-
-    #try the door again
-    door.interact(player) #now should be unlocked and player walks through
-
-    #collect the star
-    star.interact(player) #player collects star and wins the game
+    pass
 
 def main():
     start_game()
